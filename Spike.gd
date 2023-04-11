4cf49cf283b80e9d273b51beb81a80453773af16
brandonnn1 <124739157+brandonnn1@users.noreply.github.com>
2023-04-11 16:09:39 +0000

    MOVING SPIKES
    FINALLY WORKING!!!

---
+++ Spike.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+var speed: float = 400
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	position.x -= speed * delta
+
+func _on_area_entered(_area):
+	queue_free()
+
+func _on_body_entered(_body):
+	queue_free()
+
+func _on_notifier_screen_exited():
+		queue_free() 
