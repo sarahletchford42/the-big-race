7a6c7b352a523ef047f5ccabc1dde037578c8942
Yeetcoeschann <jlewis.cw743@gmail.com>
2023-04-13 17:05:37 +0000

    Fixed BG scrolling and ui scaling
    BG scrolling now works without a shader!

---
+++ scenes/background_image1.gd
@@ -0,0 +1,16 @@
+extends Sprite2D
+var speed: float = -5
+var g_texture_width: float = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	g_texture_width = texture.get_size().x * scale.x
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	position.x += speed
+	_attempt_reposition()
+	
+func _attempt_reposition():
+	if position.x < -g_texture_width:
+		position.x += 2 * g_texture_width
